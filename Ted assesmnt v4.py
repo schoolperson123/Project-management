f1755a6abd5c33e80edd550cc8ec2b10de70373a
schoolperson123 <81541661+schoolperson123@users.noreply.github.com>
2021-06-08 12:14:44 +1200

    Add files via upload

--- Ted assesmnt v4.py
+++ Ted assesmnt v4.py
@@ -1,38 +1,58 @@
 # Imports
 from appJar import gui
 # Variables
-menu_list = [
-    {"id": 1, "name": "cheese burger", "price": 34, "$": "USD"},
-    {"id": 2, "name": "XL cheese burger", "price": 23, "$": "USD"},
-    {"id": 3, "name": "bacon sandwich", "price": 65, "$": "USD"},
-    {"id": 4, "name": "Pepsi", "price": 66, "$": "USD"},
-    {"id": 5, "name": "Coke", "price": 21, "$": "USD"}]
+food_list = []
 Order_list = []
-def changed(props):
-    print("Changed", props)
-toppings={"Cheese":False, "Tomato":False, "Bacon":False,
-            "Corn":False, "Mushroom":False}
+price = 0
+limit = 0
 
 # Functions
-class Gui:
+class Menu:
     """
     Class to set up and display gui
     Also manages the button events
     """
-    def __init__(self):
+    def __init__(self, name, price, food):
         """
                 Initialise the gui class in 2 columns including:
                 - The top bar
                 - the data collection frame
                 - the data display window
         """
+        self._name = name
+        self._price = price
+        self._food_type = food
+
+    def get_name(self):
+            return (self._name)
+
+    def get_price(self):
+            return (self._price)
+
+    def get_food(self):
+            return (self._food)
+
+food_list.append(Menu('Cheese Burger', 14, 'Burger'))
+food_list.append(Menu('Chicken Burger', 14, 'Burger'))
+food_list.append(Menu('Beef Burger', 12, 'Burger'))
+food_list.append(Menu('Fish Burger', 11, 'Burger'))
+food_list.append(Menu('Water', 1, 'Drink'))
+food_list.append(Menu('Milk', 14, 'Drink'))
+food_list.append(Menu('Coke', 12, 'Drink'))
+food_list.append(Menu('Orange', 13, 'Drink'))
+food_list.append(Menu('Chips', 14, 'Sides'))
+food_list.append(Menu('Orange', 14, 'Sides'))
+food_list.append(Menu('Salad', 9, 'Sides'))
+food_list.append(Menu('Nugget', 14, 'Sides'))
+
+class Gui:
+    def __init__(self):
         app = gui("Gui fast food menu", "1750x920")
         self._app = app
         app.addLabel("title", colspan=2)
         app.setLabelBg("title", "blue")
         app.setLabelFg("title", "orange")
-        for menu in menu_list:
-            app.addLabel("f1", menu.get('name'), menu.get('price'), menu.get('$'))
+
         with app.frame("Back"):
             app.addButton("Back", self.press)
 
@@ -58,10 +78,7 @@ class Gui:
                 app.addButtons(["burger", "fries", "melon"], self.press)
                 app.setBg("lightBlue")
                 app.setFont(20)
-                app.startToggleFrame("Toppings")
-                app.addProperties("Toppings", toppings)
-                app.setPropertiesChangeFunction("Toppings", changed)
-                app.stopToggleFrame()
+
 
             with app.frame("data_display"):
                 """
@@ -76,8 +93,10 @@ class Gui:
                 app.setFont(20)
 
 
+
         app.firstFrame("stack")  # Sets the first frame added to stack to display first
         app.go()
+        
     def start_app(self):
         self._app.go()
     def press(self, btn):
@@ -89,6 +108,14 @@ class Gui:
             self._app.selectFrame("stack", 2)
         elif btn == "burger":
             self._app.selectFrame("stack", 3)
+
+    def ordering_system(self, btn):
+        while True:
+            if btn == menu_list(1):
+                Order_list.append(self, 1)
+                print(Order_list)
+
+
 
 
 # main routine
